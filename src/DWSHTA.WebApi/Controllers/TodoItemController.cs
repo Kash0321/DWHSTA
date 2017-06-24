59bba5d594b86b24299900994b1eba42d1ccd1e9
Kash0321 <castillo.sergio@gmail.com>
2017-06-24 16:34:59 +0200

    Softdelete, POF

--- src/DWSHTA.WebApi/Controllers/TodoItemController.cs
+++ src/DWSHTA.WebApi/Controllers/TodoItemController.cs
@@ -15,7 +15,7 @@ namespace DWSHTA.WebApi.Controllers
         {
             base.Initialize(controllerContext);
             MobileServiceContext context = new MobileServiceContext();
-            DomainManager = new EntityDomainManager<TodoItem>(context, Request);
+            DomainManager = new EntityDomainManager<TodoItem>(context, Request, enableSoftDelete: true);
         }
 
         // GET tables/TodoItem
