f83c74c1ebe3e1fbba9959c7b388df5f209ec57a
Kash0321 <castillo.sergio@gmail.com>
2017-07-24 22:00:48 +0200

    Ajustes  menores

---
+++ src/DWHSTA.Mobile/DWHSTA/Infrastructure/Check.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWHSTA.Infrastructure
+{
+    public static class Check
+    {
+        public static void IsNull(string target, string name)
+        {
+            if (string.IsNullOrWhiteSpace(target))
+                throw new ArgumentNullException(target, name);
+        }
+    }
+}
---
+++ src/DWHSTA.Mobile/DWHSTA/NewTableOrder/NewTableOrderView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DWHSTA.NewTableOrder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTableOrderView : ContentPage
+    {
+        public NewTableOrderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
