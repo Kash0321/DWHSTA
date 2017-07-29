57c8f0132dd7db39d17e1ae6381a65d055ce25cc
Kash0321 <castillo.sergio@gmail.com>
2017-07-29 17:51:25 +0200

    Implementación de ImageButton completada y funcional en la APP

---
+++ src/DWHSTA.Mobile/DWHSTA/Infrastructure/ImageButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DWHSTA.Infrastructure
+{
+    /// <summary>
+    /// Enhances <see cref="Image"/> control to be <see cref="Command"/>-available, mixing functionality of <see cref="Button"/> and <see cref="Image"/>
+    /// </summary>
+    public class ImageButton : Image
+    {
+        /// <summary>
+        /// <see cref="Command"/> to be executed when user taps on the <see cref="ImageButton"/>
+        /// </summary>
+        public static BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(Command), typeof(ImageButton));
+
+        /// <summary>
+        /// <see cref="Command"/> to be executed when user taps on the <see cref="ImageButton"/>
+        /// </summary>
+        public Command Command
+        {
+            get { return (Command)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="ImageButton"/>
+        /// </summary>
+        public ImageButton()
+        {
+            GestureRecognizers.Add(new TapGestureRecognizer() { Command = new Command(async (s) => { await DisTap(s); }) });
+        }
+
+        private async Task DisTap(object sender)
+        {
+            Opacity = .5;
+            await Task.Delay(200);
+
+            if (Command != null)
+            {
+                Command.Execute(sender);
+            }
+
+            Opacity = 1;
+        }
+    }
+}
